85d8558da896ff8354df08e3b8482c8f939b01af
ThomasMoreno19 <thomasmoreno004@gmail.com>
2026-07-25 20:20:40 -0300

    US: Asignar usuario como administrador, toma el id de grupo y el id del usuario que lo crea y crea una instancia de Member con el rol ADMIN

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GroupModule } from './group/group.module';
-import { PointOfInterestModule } from './point-of-interest/point-of-interest.module';
 import { GroupMembershipModule } from './group-membership/group-membership.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [GroupModule, PointOfInterestModule, GroupMembershipModule],
+  imports: [GroupModule, GroupMembershipModule],
   controllers: [AppController],
   providers: [AppService],
 })
